d54f365e481da07d59e2ffae60dc55c742277561
Bazil Korath <korathbasil@live.com>
2021-06-10 23:37:21 +0530

    user signup works , need to add user details to firestore

--- src/Components/Signup/Signup.js
+++ src/Components/Signup/Signup.js
@@ -1,22 +1,60 @@
-import React from 'react';
+import {useState} from 'react';
+import {Link, useHistory} from 'react-router-dom';
+import {auth} from '../../firebase';
 
 import Logo from '../../olx-logo.png';
 import './Signup.css';
 
 export default function Signup() {
+  const history = useHistory();
+  const [signupInput, setsignupInput] = useState({
+    name: "",
+    email: "",
+    phone: "",
+    password: ""
+  });
+  
+  const inputChangeHandler = (e) => {
+    setsignupInput({
+      ...signupInput,
+      [e.target.name]: e.target.value
+    })
+  }
+
+  const doSignup = (e) => {
+    e.preventDefault();
+    auth.createUserWithEmailAndPassword(signupInput.email, signupInput.password)
+    .then((result) => {
+      result.user.updateProfile({displayName: signupInput.name})
+      .then(user => console.log(user))
+      return
+    })
+    .then(() => {
+      
+      setsignupInput({
+        name: "",
+        email: "",
+        phone: "",
+        password: ""
+      });
+      // history.push('/')
+      alert("done")
+    })
+    .catch((e) => console.log(e))
+}
   return (
     <div>
       <div className="signupParentDiv">
         <img width="200px" height="200px" src={Logo}></img>
         <form>
-          <label htmlFor="fname">Username</label>
+          <label htmlFor="fname">Name</label>
           <br />
           <input
             className="input"
             type="text"
-            id="fname"
             name="name"
-            defaultValue="John"
+            value={signupInput.name}
+            onChange={(e) => inputChangeHandler(e)}
           />
           <br />
           <label htmlFor="fname">Email</label>
@@ -24,9 +62,9 @@ export default function Signup() {
           <input
             className="input"
             type="email"
-            id="fname"
             name="email"
-            defaultValue="John"
+            value={signupInput.email}
+            onChange={(e) => inputChangeHandler(e)}
           />
           <br />
           <label htmlFor="lname">Phone</label>
@@ -34,9 +72,9 @@ export default function Signup() {
           <input
             className="input"
             type="number"
-            id="lname"
             name="phone"
-            defaultValue="Doe"
+            value={signupInput.phone}
+            onChange={(e) => inputChangeHandler(e)}
           />
           <br />
           <label htmlFor="lname">Password</label>
@@ -44,15 +82,17 @@ export default function Signup() {
           <input
             className="input"
             type="password"
-            id="lname"
             name="password"
-            defaultValue="Doe"
+            value={signupInput.password}
+            onChange={(e) => inputChangeHandler(e)}
           />
           <br />
           <br />
-          <button>Signup</button>
+          <button onClick={doSignup}>Signup</button>
         </form>
+        <Link to="/login">
         <a>Login</a>
+        </Link>
       </div>
     </div>
   );
