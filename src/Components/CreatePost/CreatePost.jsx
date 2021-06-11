710ceff749743f39eafd03f4e0d3e920cc8d1dce
Bazil Korath <korathbasil@live.com>
2021-06-11 23:06:45 +0530

    basic form designed for create post

--- src/Components/CreatePost/CreatePost.jsx
+++ src/Components/CreatePost/CreatePost.jsx
@@ -1,13 +1,51 @@
-import styles from './CreatePost.module.css';
+import { useState } from "react";
+import {storage} from '../../firebase';
+import styles from "./CreatePost.module.css";
 
-const CreatePost = () => (
+const CreatePost = () => {
+    const [itemImage, setItemImage] = useState(null);
+
+    const uploadImage = () => {
+        storage.ref(`/products/${itemImage.name}`).put(itemImage)
+        .then(snapshot => {
+            console.log(snapshot)
+        })
+    }
+
+  return (
     <div className={styles.createPost}>
-        <div className={styles.createPostChild}>
-            <h2>POST YOUR AD</h2>
-            
-
-        </div>
+      <div className={styles.createPostChild}>
+        <h2>POST YOUR AD</h2>
+        <form className={styles.createPostForm}>
+          <h2>INCLUDE SOME DETAILS</h2>
+          <div className={styles.formInputWrapper}>
+            <p>Ad title</p>
+            <input type="text" />
+            <p>Mention the key features of your item</p>
+          </div>
+          <div className={styles.formInputWrapper}>
+            <p>Description</p>
+            <input type="text" />
+            <p>Include condition, features and reason of selling</p>
+          </div>
+          <h2>SET A PRICE</h2>
+          <div className={styles.formInputWrapper}>
+            <p>Price</p>
+            <input type="text" />
+          </div>
+          <h2>UPLOAD IMAGE</h2>
+          <div className={styles.formInputWrapper}>
+            {itemImage && <img className={styles.productImage} src={itemImage} alt="" />}
+            <input onChange={(e) => setItemImage(URL.createObjectURL(e.target.files[0]))} type="file" />
+          </div>
+          <div className={styles.formInputWrapper}>
+            <button onClick={uploadImage}>Post now</button>
+          </div>
+        </form>
+        <div className={styles.formContainer}></div>
+      </div>
     </div>
-)
+  );
+};
 
 export default CreatePost;
