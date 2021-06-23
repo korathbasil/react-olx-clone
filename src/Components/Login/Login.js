9946dea83850087bc72323e9291f22ac98ffcb17
Bazil Korath <korathbasil@live.com>
2021-06-23 20:17:16 +0530

    page selection in login works

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 import styles from "./Login.module.css";
 import EmailLogin from "./EmailLogin";
 import EmailSignup from "./EmailSignup";
@@ -5,24 +7,36 @@ import GoogleIcon from "../../assets/GoogleIcon";
 import FacebookIcon from "../../assets/FacebookIcon";
 
 const Login = () => {
+  const [openedPage, setOpenedPage] = useState("");
+  const openedPageHandler = (page) => {
+    setOpenedPage(page);
+  };
   return (
     <div className={styles.loginParent}>
       <div className={styles.login}>
-        {/* <div className={styles.linksContainer}>
-          <button>Login with Email</button>
-          <button>
-            <GoogleIcon />
-            Continue with Google
-          </button>
-          <button>
-            <FacebookIcon />
-            Continue with facebook
-          </button>
-          <h4>OR</h4>
-          <button>Signup with Email</button>
-        </div> */}
-        {/* <EmailLogin /> */}
-        <EmailSignup />
+        {openedPage === "" && (
+          <div className={styles.loginHome}>
+            <div className={styles.linksContainer}>
+              <button onClick={() => openedPageHandler("login")}>
+                Login with Email
+              </button>
+              <button>
+                <GoogleIcon />
+                Continue with Google
+              </button>
+              <button>
+                <FacebookIcon />
+                Continue with facebook
+              </button>
+              <h4>OR</h4>
+              <button onClick={() => openedPageHandler("signup")}>
+                Signup with Email
+              </button>
+            </div>
+          </div>
+        )}
+        {openedPage === "login" && <EmailLogin />}
+        {openedPage === "signup" && <EmailSignup />}
       </div>
     </div>
   );
