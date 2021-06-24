0d106ab3eb43dede88e0c6859845307708d9e81b
Bazil Korath <korathbasil@live.com>
2021-06-24 19:24:10 +0530

    designed profile picture and edit button in profile page

--- src/Pages/Profile/Profile.js
+++ src/Pages/Profile/Profile.js
@@ -1,10 +1,27 @@
 import styles from "./Profile.module.css";
 import Header from "../../Components/Header/Header";
+import EditIcon from "../../assets/EditIcon";
+import Footer from "../../Components/Footer/Footer";
 
 const Profile = () => {
   return (
     <div className={styles.profileParent}>
       <Header />
+      <div className={styles.profileChild}>
+        <div className={styles.left}>
+          <div className={styles.iamgeContainer}>
+            <img
+              src="https://images.unsplash.com/photo-1531427186611-ecfd6d936c79?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZmlsZSUyMHBpY3R1cmV8ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80"
+              alt=""
+            />
+            <div>
+              <EditIcon />
+            </div>
+          </div>
+        </div>
+        <div className={styles.right}></div>
+      </div>
+      <Footer />
     </div>
   );
 };
