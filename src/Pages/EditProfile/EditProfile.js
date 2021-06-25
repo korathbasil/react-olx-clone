b12c163e339315467299f7c0c2774c52df12828f
Bazil Korath <korathbasil@live.com>
2021-06-25 20:55:32 +0530

    navigation bw profile picture and info in editProfile page is done

--- src/Pages/EditProfile/EditProfile.js
+++ src/Pages/EditProfile/EditProfile.js
@@ -1,3 +1,5 @@
+import { Route, Switch, Link } from "react-router-dom";
+
 import styles from "./EditProfile.module.css";
 import Header from "../../Components/Header/Header";
 
@@ -7,11 +9,24 @@ const EditProfile = () => {
       <Header />
       <div className={styles.editProfileChild}>
         <div className={styles.left}>
-          <p>Edit Profile</p>
-          <p>Edit Profile Picture</p>
+          <Link to="/editProfile/info">
+            <p>Edit Profile</p>
+          </Link>
+          <Link to="/editProfile/picture">
+            <p>Edit Profile Picture</p>
+          </Link>
           <button>View Profile</button>
         </div>
-        <div className={styles.right}></div>
+        <div className={styles.right}>
+          <Switch>
+            <Route path="/editProfile/info">
+              <h2>Profile</h2>
+            </Route>
+            <Route path="/editProfile/picture">
+              <h2>Profile Picture</h2>
+            </Route>
+          </Switch>
+        </div>
       </div>
     </div>
   );
