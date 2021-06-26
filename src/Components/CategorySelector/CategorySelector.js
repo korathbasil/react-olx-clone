d02ec3c02d7885835657371db2121b07cdad3e58
Bazil Korath <korathbasil@live.com>
2021-06-26 21:17:40 +0530

    selecting categories works

--- src/Components/CategorySelector/CategorySelector.js
+++ src/Components/CategorySelector/CategorySelector.js
@@ -6,12 +6,26 @@ import Arrow from "../../assets/Arrow";
 
 const CategorySelector = () => {
   const [activeMenu, setActiveMenu] = useState(null);
+
+  const activeMenuHandler = (name, subs) => {
+    if (activeMenu?.name === name) {
+      return setActiveMenu(null);
+    }
+    setActiveMenu({
+      name: name,
+      subs: subs,
+    });
+  };
+
   return (
     <div className={styles.categories}>
       <div className={styles.left}>
-        {categories.map(({ name, Icon }) => {
+        {categories.map(({ name, Icon, subs }) => {
           return (
-            <div className={styles.category}>
+            <div
+              className={styles.category}
+              onClick={() => activeMenuHandler(name, subs)}
+            >
               <div>
                 {Icon && <Icon />}
                 <p>{name}</p>
@@ -19,6 +33,17 @@ const CategorySelector = () => {
               <div className={styles.arrowContainer}>
                 <Arrow />
               </div>
+              {activeMenu?.name === name && (
+                <div className={styles.subCategoryContainer}>
+                  {subs.map((item) => {
+                    return (
+                      <div className={styles.subCategory}>
+                        <p>{item.name}</p>
+                      </div>
+                    );
+                  })}
+                </div>
+              )}
             </div>
           );
         })}
