2a57346357a54ec8b9c90a7d731d562030cc4f7d
Bazil Korath <korathbasil@live.com>
2021-06-26 19:18:20 +0530

    added subcategories in categories file

--- src/categories.js
+++ src/categories.js
@@ -9,38 +9,239 @@ export default categories = [
             name="For Sale: Houses & Appartments",
         },
         {
-            name="For Sale: Houses & Appartments",
+            name="For Rent: Houses & Appartments",
+        },
+        {
+            name="Lands & Plosts",
+        },
+        {
+            name="For Rent: Shops & Offices",
+        },
+        {
+            name="For Sale: Shops & Offices",
+        },
+        {
+            name="PG & guest Houses",
         },
     ],
   },
   {
     name: "Mobiles",
+    subs: [
+      {
+        name="Mobile Phones",
+      },
+      {
+        name="Accessories",
+      },
+      {
+        name="Tablets",
+      },
+    ]
   },
   {
     name: "Jobs",
+    subs: [
+      {
+        name="Data Entry & Back office",
+      },
+      {
+        name="Sales & Marketing",
+      },
+      {
+        name="BPO & Telecaller",
+      },
+      {
+        name="Driver",
+      },
+      {
+        name="Office Assistant",
+      },
+      {
+        name="Delivery & Collection",
+      },
+      {
+        name="Teacher",
+      },
+      {
+        name="Cook",
+      },
+      {
+        name="Receptionist & Front Office",
+      },
+      {
+        name="Operator & Technician",
+      },
+      {
+        name="IT Engineer & Developer",
+      },
+      {
+        name="Hotel & Travel Executive",
+      },
+      {
+        name="Accountant",
+      },
+      {
+        name="Designer",
+      },
+      {
+        name="Other Jobs",
+      },
+    ]
   },
   {
     name: "Bikes",
+    subs: [
+      {
+        name="Motorcycles",
+      },
+      {
+        name="Scooters",
+      },
+      {
+        name="Spare Parts",
+      },
+      {
+        name="Bicycles",
+      },
+    ]
   },
   {
     name: "Electronics & Appliances",
+    subs: [
+      {
+        name="TVs, Video - Audio",
+      },
+      {
+        name="Kitchen & Other Appliances",
+      },
+      {
+        name="Computers & Laptops",
+      },
+      {
+        name="Computer Accessories",
+      },
+      {
+        name="Hard Disks , Printers & Monitors",
+      },
+      {
+        name="Cameras & Lenses",
+      },
+      {
+        name="Games & Entertainment",
+      },
+      {
+        name="Fridges",
+      },
+      {
+        name="ACs",
+      },
+      {
+        name="Washing Machines",
+      },
+    ]
   },
   {
     name: "Commercial Vehicles & Spares",
+    subs: [
+      {
+        name="Commercial & Other Vehicles",
+      },
+      {
+        name="Spare Parts",
+      },
+    ]
   },
   {
     name: "Furniture",
+    subs: [
+      {
+        name="Sofa & Dining",
+      },
+      {
+        name="Beds & Wardrobs",
+      },
+      {
+        name="Home Decor & Garden",
+      },
+      {
+        name="Kids Furniture",
+      },
+      {
+        name="Other Household Items",
+      },
+    ]
   },
   {
     name: "Fashion",
+    subs: [
+      {
+        name="Men",
+      },
+      {
+        name="Women",
+      },
+      {
+        name="Kids",
+      },
+
+    ]
   },
   {
     name: "Books, Sports & Hobbies",
+    subs: [
+      {
+        name="Books",
+      },
+      {
+        name="gym & Fitness",
+      },
+      {
+        name="Musical Instruments",
+      },
+      {
+        name="Sports Equipment",
+      },
+      {
+        name="Other Hobbies",
+      },
+    ]
   },
   {
     name: "Pets",
+    subs: [
+      {
+        name="Fishes & Aquarium",
+      },
+      {
+        name="Pet Food & Accessories",
+      },
+      {
+        name="Dogs",
+      },
+      {
+        name="Other Pets",
+      },
+    ]
   },
   {
     name: "Services",
+    subs: [
+      {
+        name="Electonics & Computer",
+      },
+      {
+        name="Education & Classes",
+      },
+      {
+        name="Drivers & Taxi",
+      },
+      {
+        name="Health & Beauty",
+      },
+      {
+        name="Other Services",
+      },
+    ]
   },
 ];
