394fe9ea3142c0b08cbd55198842cf20dde98112
Bazil Korath <korathbasil@live.com>
2021-06-26 18:20:53 +0530

    added categories file, not copmpleted

---
+++ src/categories.js
@@ -0,0 +1,46 @@
+export default categories = [
+  {
+    name: "Cars",
+  },
+  {
+    name: "Properties",
+    subs: [
+        {
+            name="For Sale: Houses & Appartments",
+        },
+        {
+            name="For Sale: Houses & Appartments",
+        },
+    ],
+  },
+  {
+    name: "Mobiles",
+  },
+  {
+    name: "Jobs",
+  },
+  {
+    name: "Bikes",
+  },
+  {
+    name: "Electronics & Appliances",
+  },
+  {
+    name: "Commercial Vehicles & Spares",
+  },
+  {
+    name: "Furniture",
+  },
+  {
+    name: "Fashion",
+  },
+  {
+    name: "Books, Sports & Hobbies",
+  },
+  {
+    name: "Pets",
+  },
+  {
+    name: "Services",
+  },
+];
