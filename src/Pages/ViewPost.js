ac3b8076743c6db062b9d9c3686067a26df52a5a
Bazil Korath <korathbasil@live.com>
2021-06-30 18:32:03 +0530

    removed overflow x bug

--- src/Pages/ViewPost.js
+++ src/Pages/ViewPost.js
@@ -1,15 +1,17 @@
-import React from 'react'
+import React from "react";
 
-import Header from '../Components/Header/Header'
-import View from '../Components/View/View'
+import Header from "../Components/Header/Header";
+import View from "../Components/View/View";
+import Footer from "../Components/Footer/Footer";
 
 function ViewPost(props) {
-    return (
-        <div>
-            <Header />
-            <View/>
-        </div>
-    )
+  return (
+    <div>
+      <Header />
+      <View />
+      <Footer />
+    </div>
+  );
 }
 
-export default ViewPost
+export default ViewPost;
