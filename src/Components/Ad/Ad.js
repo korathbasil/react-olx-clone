4dd981f4c321f7b95599d26cc2ab95af053d9634
Bazil Korath <korathbasil@live.com>
2021-06-30 18:52:31 +0530

    added ad component file

---
+++ src/Components/Ad/Ad.js
@@ -0,0 +1,11 @@
+import styles from "./Ad.module.css";
+
+const Ad = () => {
+  return (
+    <div className={styles.ad}>
+      <p>Iam ad</p>
+    </div>
+  );
+};
+
+export default Ad;
--- src/Components/Feed/Feed.js
+++ src/Components/Feed/Feed.js
@@ -1,10 +1,14 @@
 import styles from "./Feed.module.css";
+import Ad from "../Ad/Ad";
 
 const Feed = () => {
   return (
     <div className={styles.feedParent}>
       <div className={styles.feedChild}>
         <h2>Iam Feed Components</h2>
+        <div className={styles.adsContainer}>
+          <Ad />
+        </div>
       </div>
     </div>
   );
