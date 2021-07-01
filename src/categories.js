40b146515b3c55978ea55d7f21b56953c62893f4
Bazil Korath <korathbasil@live.com>
2021-07-01 23:31:02 +0530

    added attributes for houses for rent

--- src/categories.js
+++ src/categories.js
@@ -136,6 +136,76 @@ export const categories = [
       },
       {
         name: "For Rent: Houses & Appartments",
+        attributes: [
+          {
+            name: "Type",
+            type: "radio",
+            options: [
+              "Apartments",
+              "Builder Floors",
+              "Farm Houses",
+              "Houses & Villas",
+            ],
+          },
+          {
+            name: "Bedrooms",
+            type: "radio",
+            options: ["1", "2", "3", "4", "4+"],
+          },
+          {
+            name: "Bathrooms",
+            type: "radio",
+            options: ["1", "2", "3", "4", "4+"],
+          },
+          {
+            name: "Furnishing",
+            type: "radio",
+            options: ["Furnished", "Semi-Furnished", "Unfurnished"],
+          },
+          {
+            name: "Listed by",
+            type: "radio",
+            options: ["Builder", "Dealer", "Owner"],
+          },
+          {
+            name: "Super Builtup Area (ft)",
+            type: "input",
+          },
+          {
+            name: "Carpet Area (ft)",
+            type: "input",
+          },
+          {
+            name: "Bachelors Allowed",
+            type: "radio",
+            options: ["No", "Yes"],
+          },
+          {
+            name: "Miantenance (Monthly)",
+            type: "input",
+          },
+          {
+            name: "Total Floors",
+            type: "input",
+          },
+          {
+            name: "Floor No.",
+            type: "input",
+          },
+          {
+            name: "Car Parking",
+            type: "radio",
+            options: ["0", "1", "2", "3", "3+"],
+          },
+          {
+            name: "Facing",
+            type: "input",
+          },
+          {
+            name: "Project Name",
+            type: "input",
+          },
+        ],
       },
       {
         name: "Lands & Plosts",
