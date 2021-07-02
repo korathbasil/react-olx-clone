1a62977239ec570147bac68b0c96984a32de7319
Bazil Korath <korathbasil@live.com>
2021-07-03 02:15:47 +0530

    added attributes office teacher job

--- src/categories.js
+++ src/categories.js
@@ -595,6 +595,26 @@ export const categories = [
       },
       {
         name: "Teacher",
+        attributes: [
+          {
+            name: "Salary Period",
+            type: "radio",
+            options: ["Hourly", "Monthly", "Weekly", "Yearly"],
+          },
+          {
+            name: "Position Type",
+            type: "radio",
+            options: ["Contract", "Full-Time", "Part-Time", "Temporary"],
+          },
+          {
+            name: "Salary From",
+            type: "input",
+          },
+          {
+            name: "Salary To",
+            type: "input",
+          },
+        ],
       },
       {
         name: "Cook",
