f9939477c272009ec6556f5008197ba07902e94c
Bazil Korath <korathbasil@live.com>
2021-07-03 02:18:29 +0530

    added attributes for accountant job

--- src/categories.js
+++ src/categories.js
@@ -733,6 +733,26 @@ export const categories = [
       },
       {
         name: "Accountant",
+        attributes: [
+          {
+            name: "Salary Period",
+            type: "radio",
+            options: ["Hourly", "Monthly", "Weekly", "Yearly"],
+          },
+          {
+            name: "Position Type",
+            type: "radio",
+            options: ["Contract", "Full-Time", "Part-Time", "Temporary"],
+          },
+          {
+            name: "Salary From",
+            type: "input",
+          },
+          {
+            name: "Salary To",
+            type: "input",
+          },
+        ],
       },
       {
         name: "Designer",
