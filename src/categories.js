b13caf50fc1aeef54c44a9d6be3deefb740b170f
Bazil Korath <korathbasil@live.com>
2021-07-03 02:18:10 +0530

    added attributes for hotel and travel exec

--- src/categories.js
+++ src/categories.js
@@ -710,6 +710,26 @@ export const categories = [
       },
       {
         name: "Hotel & Travel Executive",
+        attributes: [
+          {
+            name: "Salary Period",
+            type: "radio",
+            options: ["Hourly", "Monthly", "Weekly", "Yearly"],
+          },
+          {
+            name: "Position Type",
+            type: "radio",
+            options: ["Contract", "Full-Time", "Part-Time", "Temporary"],
+          },
+          {
+            name: "Salary From",
+            type: "input",
+          },
+          {
+            name: "Salary To",
+            type: "input",
+          },
+        ],
       },
       {
         name: "Accountant",
