810afde5f462f1071d03fcc4f4e874009955e12c
Bazil Korath <korathbasil@live.com>
2021-07-04 21:12:12 +0530

    added dynamic image into ad comp.

--- src/Components/Ad/Ad.js
+++ src/Components/Ad/Ad.js
@@ -4,10 +4,7 @@ const Ad = ({ ad }) => {
   return (
     <div className={styles.ad}>
       <div className={styles.adTop}>
-        <img
-          src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fprestigeportraits.com%2Fwp-content%2Fthemes%2Fprestige%2Fassets%2Fbuild%2Fimages%2Fgalleries%2Fgallery-2%2Fgallery-image-3.jpg&f=1&nofb=1"
-          alt=""
-        />
+        <img src={ad.imageUrl[0]} alt="" />
       </div>
       <div className={styles.adBottom}>
         <h3>₹ {ad.price}</h3>
