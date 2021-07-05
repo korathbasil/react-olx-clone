add23a61258a494aa3c037d0edf7ecd305de7cd7
Bazil Korath <korathbasil@live.com>
2021-07-06 00:41:59 +0530

    footer top designed except for social icons and app store links

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -2,12 +2,44 @@ import styles from "./Footer.module.css";
 
 const Footer = () => {
   return (
-    <div className={styles.footerParent}>
+    <div className={styles.footer}>
       <div className={styles.top}>
-        <div className={styles.footerChild}></div>
+        <div className={styles.container}>
+          <div className={styles.linkCoumns}>
+            <h4>POPULAR LOCATIONS</h4>
+            <p>Kolkata</p>
+            <p>Mumbai</p>
+            <p>Chennai</p>
+            <p>Pune</p>
+          </div>
+          <div className={styles.linkCoumns}>
+            <h4>TRENDING LOCATIONS</h4>
+            <p>Bhuwaneshwar</p>
+            <p>Hyderabad</p>
+            <p>Chandigarh</p>
+            <p>Nashik</p>
+          </div>
+          <div className={styles.linkCoumns}>
+            <h4>ABOUT US</h4>
+            <p>About OLX Group</p>
+            <p>Careers</p>
+            <p>Contact Us</p>
+            <p>OLX People</p>
+            <p>Wash Jobs</p>
+          </div>
+          <div className={styles.linkCoumns}>
+            <h4>OLX</h4>
+            <p>Help</p>
+            <p>Sitemap</p>
+            <p>Legal & Privacy Information</p>
+          </div>
+          <div className={styles.linkCoumns}>
+            <h4>FOLLOW US</h4>
+          </div>
+        </div>
       </div>
       <div className={styles.bottom}>
-        <div className={styles.footerChild}></div>
+        <div className={styles.container}></div>
       </div>
     </div>
   );
