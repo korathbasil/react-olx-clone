510192516bb055c93486ae3be090327fe6d64001
Bazil Korath <korathbasil@live.com>
2021-07-12 19:58:35 +0530

    empty ads designed in ads page

--- src/pages/MyAds/MyAds.js
+++ src/pages/MyAds/MyAds.js
@@ -1,3 +1,7 @@
+import { Link } from "react-router-dom";
+
+import NoAdsImage from "../../assets/images/no-ads-image.png";
+
 import styles from "./MyAds.module.css";
 import Header from "../../components/Header/Header";
 import Menu from "../../components/Menu/Menu";
@@ -7,7 +11,26 @@ const MyAds = () => {
     <div className={styles.myAds}>
       <Header />
       <Menu />
-      <p>This is my ads</p>
+      <div className={styles.myAdsChild}>
+        <div className={styles.switcher}>
+          <p>Ads</p>
+          <p>Favourites</p>
+          <div className={styles.adsContainer}>
+            <div className={styles.myAds}>
+              <div className={styles.empty}>
+                <img src={NoAdsImage} alt="" />
+                <h4>You haven't listed anything yet.</h4>
+                <span>Let go of what you</span>
+                <span>don't use anymore</span>
+                <Link to="/sell">
+                  <button>Start Selling</button>
+                </Link>
+              </div>
+            </div>
+            <div className={styles.favouriteAds}></div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
