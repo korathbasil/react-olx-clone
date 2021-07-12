7e233629bd735081392d4a5feb0158122b1387f6
Bazil Korath <korathbasil@live.com>
2021-07-12 20:37:36 +0530

    feat: geting ads on my ads page

---
+++ src/components/MiniAd/MiniAd.js
@@ -0,0 +1,7 @@
+import styles from "./MiniAd.module.css";
+
+const MiniAd = () => {
+  return <div className={styles.miniAd}>iam a mini Ad</div>;
+};
+
+export default MiniAd;
--- src/pages/MyAds/MyAds.js
+++ src/pages/MyAds/MyAds.js
@@ -1,12 +1,37 @@
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
-import NoAdsImage from "../../assets/images/no-ads-image.png";
+import { db } from "../../firebase";
+import useGlobalStore from "../../store/GlobalStore";
 
+import NoAdsImage from "../../assets/images/no-ads-image.png";
 import styles from "./MyAds.module.css";
 import Header from "../../components/Header/Header";
 import Menu from "../../components/Menu/Menu";
+import MiniAd from "../../components/MiniAd/MiniAd";
 
 const MyAds = () => {
+  const [{ user }] = useGlobalStore();
+  const [ads, setAds] = useState([]);
+
+  useEffect(() => {
+    if (user) {
+      db.collection("posts")
+        .where("userId", "==", user.id)
+        .get()
+        .then((snapshot) => {
+          let adsArray = [];
+          snapshot.forEach((doc) => {
+            console.log(doc.data());
+            adsArray.push({
+              id: doc.id,
+              ...doc.data(),
+            });
+          });
+          setAds(adsArray);
+        });
+    }
+  }, [user]);
   return (
     <div className={styles.myAds}>
       <Header />
@@ -17,15 +42,20 @@ const MyAds = () => {
           <p>Favourites</p>
           <div className={styles.adsContainer}>
             <div className={styles.myAds}>
-              <div className={styles.empty}>
-                <img src={NoAdsImage} alt="" />
-                <h4>You haven't listed anything yet.</h4>
-                <span>Let go of what you</span>
-                <span>don't use anymore</span>
-                <Link to="/sell">
-                  <button>Start Selling</button>
-                </Link>
-              </div>
+              {ads.length == 0 && (
+                <div className={styles.empty}>
+                  <img src={NoAdsImage} alt="" />
+                  <h4>You haven't listed anything yet.</h4>
+                  <span>Let go of what you</span>
+                  <span>don't use anymore</span>
+                  <Link to="/sell">
+                    <button>Start Selling</button>
+                  </Link>
+                </div>
+              )}
+              {ads.map((ad) => (
+                <MiniAd />
+              ))}
             </div>
             <div className={styles.favouriteAds}></div>
           </div>
