7896f5134df14e0448cf3902b5ffc0e067e70f17
Bazil Korath <korathbasil@live.com>
2021-07-12 21:36:49 +0530

    feat;getting ads in profile page added

--- src/pages/MyProfile/MyProfile.js
+++ src/pages/MyProfile/MyProfile.js
@@ -1,9 +1,39 @@
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+
+import { db } from "../../firebase";
+import useGlobalStore from "../../store/GlobalStore";
+
+import NoAdsImage from "../../assets/images/no-ads-image.png";
 import styles from "./MyProfile.module.css";
 import Header from "../../components/Header/Header";
 import EditIcon from "../../assets/EditIcon";
+import MiniAd from "../../components/MiniAd/MiniAd";
 import Footer from "../../components/Footer/Footer";
 
 const MyProfile = () => {
+  const [{ user }] = useGlobalStore();
+  const [ads, setAds] = useState([]);
+
+  useEffect(() => {
+    if (user) {
+      db.collection("posts")
+        .where("userId", "==", user.id)
+        .get()
+        .then((snapshot) => {
+          let adsArray = [];
+          snapshot.forEach((doc) => {
+            console.log(doc.data());
+            adsArray.push({
+              id: doc.id,
+              ...doc.data(),
+            });
+          });
+          setAds(adsArray);
+        });
+    }
+  }, [user]);
+
   return (
     <div className={styles.profileParent}>
       <Header />
@@ -22,8 +52,26 @@ const MyProfile = () => {
         </div>
         <div className={styles.right}>
           <div className={styles.rightTop}>
-            <h1>ALEX JAMES</h1>
+            <h1>{user?.displayName}</h1>
             <button>Edit Profile</button>
+          </div>
+          <div className={styles.rightAds}>
+            {ads.length == 0 && (
+              <div className={styles.empty}>
+                <img src={NoAdsImage} alt="" />
+                <h4>There is no ads</h4>
+                <p>
+                  When users post ads, will appear here. If you want to post
+                  something you can do it now
+                </p>
+                <Link to="/sell">
+                  <button>Start Selling</button>
+                </Link>
+              </div>
+            )}
+            {ads.map((ad) => (
+              <MiniAd />
+            ))}
           </div>
         </div>
       </div>
