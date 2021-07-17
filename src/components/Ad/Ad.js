7a6eeff5dc48ee2b186f6c7c07f50fe4aea1d9bc
Bazil Korath <korathbasil@live.com>
2021-07-17 20:46:28 +0530

    feat: add date formatting in ad comp. with date-fns

--- src/components/Ad/Ad.js
+++ src/components/Ad/Ad.js
@@ -1,7 +1,28 @@
+import { useEffect, useState } from "react";
+import formatDistance from "date-fns/formatDistance";
+import format from "date-fns/format";
+
 import styles from "./Ad.module.css";
 
 const Ad = ({ ad }) => {
   // console.log(ad[ad.attributes.featuredAttributes[0]]);
+  const [date, setDate] = useState();
+  useEffect(() => {
+    const dateDistance = formatDistance(new Date(ad.createdAt), new Date(), {
+      addSuffix: true,
+    });
+    if (
+      parseInt(dateDistance.split(" ")[0]) < 10 &&
+      dateDistance.split(" ")[1] === "days"
+    )
+      setDate(dateDistance);
+    else if (dateDistance.split(" ")[1] === "years") {
+      setDate(format(new Date(ad.createdAt), "MMMM dd yy"));
+    } else {
+      setDate(format(new Date(ad.createdAt), "MMMM dd"));
+    }
+  }, []);
+
   return (
     <div className={styles.ad}>
       <div className={styles.adTop}>
@@ -18,7 +39,7 @@ const Ad = ({ ad }) => {
           <p>
             {ad.address.City} {ad.address.State}
           </p>
-          <p>$ days ago</p>
+          <p>{date}</p>
         </div>
       </div>
     </div>
